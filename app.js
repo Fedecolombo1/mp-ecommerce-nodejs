1e57fdcef1ae685075f0f2a5ecba687ab88a7793
Fedecolombo1 <colombofederico17@gmail.com>
2020-12-04 19:45:40 -0300

    five

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
 
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 const mercadopago = require("mercadopago")
 
@@ -124,7 +135,7 @@ app.post("/comprar", function(req, res){
     
     })
 
-    app.get("/callback",function(req, res){
+    app.get("/callback", function(req, res){
 
         console.log("HOLAAAA" + req.query);
 
@@ -151,3 +162,21 @@ app.post("/comprar", function(req, res){
     })
 
 app.listen(port);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+  module.exports = app;
